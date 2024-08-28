4103770e6f731ce45351e7326672723a88663665
Vedant Solunke <vedantsolunke@Vedants-Air.bwrouter>
2024-08-28 15:44:40 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connection = async () => {
+  const URI =
+    "mongodb+srv://vedsocialid:admin123@cluster0.ukamajh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+  try {
+    await mongoose.connect(URI, {});
+    console.log("Connected to the database");
+  } catch (error) {
+    console.log("Error while connecting to the database", error);
+  }
+};
+
+export default connection;
